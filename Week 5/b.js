b1eef987c1bb7e27f2cb9d92182e74b0addf8ab8
lideta <bele2006ec@gmail.com>
2018-08-02 22:22:51 +0300

    week 5 homework

---
+++ Week 5/b.js
@@ -0,0 +1,44 @@
+Q1
+console.log("Hellow World")
+console.log("selam lezih Alem")
+console.log("Bonjour le monde")
+Q2
+console.log('I\'m awesome');
+Q3
+a. var x;
+b. console.log("noting");
+c. console.log(x);
+d. var x = 8;
+e. console.log("eight");
+f. console.log(x);
+Q4. var y = "Ethiopia";
+a. console.log("the value is Ethiopia");
+b.console.log(y);
+c.var y = "Family";
+d.console.log("it shows family");
+e.console.log(y);
+Q5 console.log(Math.round(7.25))
+a. var z = 7.25;
+b. console.log(z);
+c. var a = Math.round(z);
+d. console.log(a);
+
+e. if(z>a){
+  var n = z;
+  console.log(n);
+
+}
+if(z<a){
+  var n = a;
+  console.log(n);
+
+}
+f. console.log(n);
+Q6
+a. var people = [];
+b. console.log("nothing");
+c. console.log(people);
+d. var animals = ["lione","tiger","bear","horse","dog","beaver"]
+e. console.log(animals);
+f.animals.push("kitten")
+console.log(animals);
