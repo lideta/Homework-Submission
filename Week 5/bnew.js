2e12c4e1366e7b0c6a6eaa68b22adea55acf45bd
lideta <bele2006ec@gmail.com>
2018-08-06 16:42:40 +0300

    full answer of week 5

---
+++ Week 5/bnew.js
@@ -0,0 +1,78 @@
+Q1
+console.log("Hello World")
+console.log("Hello Alem")
+console.log("Bonjour le monde")
+
+Q2
+console.log('I\'m awesome');
+
+Q3
+a. var x;
+b. console.log("undefined");
+c. console.log(x);
+d. var x = 8;
+e. console.log("eight");
+f. console.log(x);
+
+Q4. var y = "Ethiopia";
+a. console.log('the value of the string is "Ethiopia"');
+b.console.log(y);
+c.var y = "Family";
+d.console.log("it shows 'Family'");
+e.console.log(y);
+
+Q5 console.log(Math.round(7.25))
+a. var z = 7.25;
+b. console.log(z);
+c. var a = Math.round(z);
+d. console.log(a);
+
+e. if(z>a){
+  var n = z;
+  console.log(n);
+
+}
+if(z<a){
+  var n = a;
+  console.log(n);
+  
+}
+f. console.log(n);
+
+Q6
+a. var people = [];
+b. console.log("the value of the array is an empty 'array'");
+c. console.log(people);
+d. var animals = ["lion","tiger","bear","horse","dog","beaver"];
+e. console.log(animals);
+f.animals.push("kitten")
+g.console.log(animals);
+
+Q7. let myString = "this is a test";
+a. console.log(myString);
+b. the way to get the length of myString is, we use myString.length. 
+c. console.log(myString.length);
+
+Q8. var x = 7;
+    var x = x%3;
+	console.log("the new value of x is 1")
+a. var x = 9;
+
+   console.log(x=x%5);
+   var y = 11;
+   console.log(y = y%8);
+   var t = 23;
+   console.log(t = t%15)
+  Q9. 
+  a. yes, yes
+   var animals = ["lion","34","45","tiger","bear","horse","dog","beaver"];
+   console.log(animals)
+  b. yes, yes, we can test by Boolean
+     var x = 6/0;
+	 var y = 10/0;
+ c.  console.log(x===y)
+   
+   	
+    	
+ 
+
